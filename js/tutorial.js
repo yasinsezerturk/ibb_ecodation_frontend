3cccbc81c0b6d11be59558414ee57ac97bbb6985
yasinsezerturk <yasinsezerturk@gmail.com>
2023-07-04 20:26:31 +0300

    cast, let, var

---
+++ js/tutorial.js
@@ -0,0 +1,183 @@
+// JS 1995 yılında Brandan Eich tarafından yazılıyor.
+// Cross platform
+// OOP uygun.
+
+//single Comment
+/*
+multiple Comment
+*/
+
+// console.log("loglama");
+// console.info("info");
+// console.warn("warn");
+// console.error("error");
+
+// alert("pop-up bilgilendirme-1");
+// window.alert("pop-up bilgilendirme-2");
+// window.document.writeln("ekranda yazı");
+
+// Variable
+// var result=4423;
+// console.log(typeof result+" => "+result);
+
+// var result=44.23;
+// console.log(typeof result+" => "+result);
+
+// var result="Js öğreniyorum";
+// console.log(typeof result+" => "+result);
+
+// var result=true;
+// console.log(typeof result+" => "+result);
+
+// Naming Convensition
+// var deneme="JS Data";
+// console.log(deneme);
+
+// var deneme44="JS Data";
+// console.log(deneme44);
+
+// var $deneme44="JS Data";
+// console.log($deneme44);
+
+// var _$deneme44="JS Data";
+// console.log(_$deneme44);
+
+// var denemeVerisi="JS Data";
+// console.log(denemeVerisi);
+
+//var &~44$deneme44="JS Data";
+//console.log(44$deneme44);
+
+// interpreter, compiler 
+// senkron, asenkron ?
+
+// whoisting
+// result2=56;
+// var result2;
+
+///////////////////////////////////////////////////
+// Operators
+// + - * ** / %
+// ! DEĞİL &&=VE ||=VEYA
+// x++
+// x--
+// ++x
+// --x
+
+// =   ATAMA
+// ==  EŞİT  (TÜRÜNE BAKMA)
+// === EŞİT  (TÜRÜNE BAK)
+
+// var number1=40;
+// var number2=5;
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
+
+// cast
+// Number
+// String()
+// var userData=Number(prompt("Lütfen sayı giriniz"));
+// console.log(userData+45);
+
+// undefined
+// var result4;
+// console.log(result4);
+
+// // NaN: Not A Number
+// var result5="asd"/4;
+// console.log(result5);
+
+// // isNaN
+// var result6=4;
+// console.log(isNaN(result6)); //false: sayıdır
+
+// // Infinity
+// console.log(4/0);
+
+///////////////////////////////////////////////////
+// Math
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.min(4,1,99,555,25,-100));
+// console.log(Math.max(4,1,99,555,25,-100));
+
+// console.log(Math.sqrt(16));
+// console.log(Math.abs(-25));
+
+// console.log(Math.pow(2,5));
+
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(3.1));
+// console.log(Math.round(6.4));
+// console.log(Math.round(6.5));
+
+// console.log(Math.sin(45));
+// console.log( Math.round(Math.random()*9+1) );
+
+//Number
+var sayi=15000;
+console.log(sayi);
+
+var sayi2=15E+3;
+console.log(sayi2);
+
+//binary, octal, decimal, hexadecimal nedir?
+
+//binary: 0-1
+var binary=0b00011
+console.log(binary)
+
+//octal:0-1-2-3-4-5-6-7
+var octal=0o7612
+console.log(octal)
+
+//decimal:0-1-2-3-4-5-6-7-8-9
+var decimal=123456
+console.log(decimal)
+
+//hexdecimal:0-1-2-3-4-5-6-7-8-9-A-B-C-D-E-F
+var hexadecimal=0xffa
+console.log(hexadecimal)
+
+
+//Number() ve parseInt() farkı nedir?
+//String() data.toString() arasındaki fark nedir?
+//Cast
+console.log('10'+10);
+
+console.log(Number('10')+10);
+
+console.log(parseInt('10')+10);
+
+console.log(String(30)+10);
+
+
+var number=10;
+console.log(number.toString()+10);
+
+
+//var, let, const
+
+var data1=11
+console.log(data1)
+
+var data1=22
+console.log(data1)
+
+let data2=33
+console.log(data2)
+
+let data3=33
+data3=44
+console.log(data3)
+
+const data4=55
+console.log(data4)
+
+const data5=55
+data5=66
+console.log(data5)
+//String
